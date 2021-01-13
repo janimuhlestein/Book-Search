66ab3075c3a479a6ee77a053562eb3019532c4d2
Jani Muhlestein <janimuhlestein@gmail.com>
2021-01-13 10:13:12 -0700

    fixed resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -56,7 +56,7 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
         },
-        removeBook: async (parent, bookId, context) => {
+        removeBook: async (parent, { bookId }, context) => {
             if(context.user) {
                 const updatedUser = await User.findByIdAndUpdate(
                     { _id: context.user._id },
