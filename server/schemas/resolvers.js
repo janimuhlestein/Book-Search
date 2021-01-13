238679bf062819f50a5419a1a7591646e999d46b
Jani Muhlestein <janimuhlestein@gmail.com>
2021-01-13 10:18:26 -0700

    minor change to removeBook

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -60,7 +60,7 @@ const resolvers = {
             if(context.user) {
                 const updatedUser = await User.findByIdAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: bookId }},
+                    { $pull: { savedBooks:{bookId: bookId } }},
                     { new: true }
                 );
                 return updatedUser;
